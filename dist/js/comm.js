0a5bd7f8d2ae039bc8824d2d3c832941b432ead5
0nejean <onejean114@gmail.com>
2023-10-30 11:43:27 +0900

    length modify

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   // fullpage
   $("#fullpage").fullpage({
-    scrollingSpeed: 1000,
+    scrollingSpeed: 700,
     // scrollBar: true,
     onLeave: function (origin, destination, direction) {
       // 빠른전환으로 이벤트중복시 fullpage와 swiper전환시점 분리막기
@@ -15,7 +15,7 @@ $(document).ready(function () {
     afterLoad: function (anchorLink, index) {
       // 전환이 끝난후 이벤트풀기
       $("#fullpage").off("scroll mousewheel");
-      if (!$(".fp-completely .swiper-wrapper").length > 0)
+      if (!$(".fp-completely .swiper-wrapper").length > 480)
         $("#fullpage").off("touchmove"); // 모바일분기
       if (swiper) swiper.mousewheel.enable();
       if (!$(".sec3").hasClass("active")) $.fn.fullpage.setAllowScrolling(true); // 슬라이드 섹션을 벗어나면 휠풀어주기
